3cfae362c82a60fc2d3a8bdf425308433fc7ad42
Bassam <bassamcctv@gmail.com>
2020-07-18 22:38:52 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-app';
+
+  name:string = '';
+
+  //header('Access-Control-Allow-Origin', '*');
+constructor (private httpClient: HttpClient)
+{}
+onNameKeyUP(event:any){
+  this.name = event.target.value;
+
 }
+getProfile()
+{
+ // this.httpClient.get('http://www.dneonline.com/calculator.asmx?op=Add&intA=1&intB=2')
+ this.httpClient.get('https://api.test.hotelbeds.com/hotel-api')
+  .subscribe(
+    (data:any[]) =>{
+      console.log(data);
+    });
+}
+}
+
